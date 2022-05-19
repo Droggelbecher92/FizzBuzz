a17077fa72475465b8f9ddd69df13790f2f5b461
Droggelbecher92 <thomaskittlaus@gmail.com>
2022-05-19 17:58:14 +0200

    3 tests green, awesome

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,11 +3,16 @@
 Für Vielfache von 5 gib „Buzz“ aus.
 Für Vielfache von 3 und 5 gib „FizzBuzz“ aus.
 * Als Antwort erwaren wir den jeweils passenden String zur gegebenen Zahl.
-* Zu testen: 3
+* Zu testen: 3 / 17 / 5
 **/
 
 public class FizzBuzz {
     public static String fizzbuzzer(int number) {
-        return null;
+        if (number%3==0){
+            return "Fizz";
+        } else if (number%5==0) {
+            return "Buzz";
+        }
+        return String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 * Für Vielfache von 3 gibt „Fizz“ aus.
 Für Vielfache von 5 gib „Buzz“ aus.
 Für Vielfache von 3 und 5 gib „FizzBuzz“ aus.
-* Zu testen: 3
+* Zu testen: 3 / 17 / 5
 **/
 class FizzBuzzTest {
 
@@ -16,6 +16,32 @@ class FizzBuzzTest {
         //Given
         int number = 3;
         String expected = "Fizz";
+
+        //When
+        String actual = FizzBuzz.fizzbuzzer(number);
+        //Then
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void shouldReturn17WithNumber17(){
+        //Given
+        int number = 17;
+        String expected = "17";
+
+        //When
+        String actual = FizzBuzz.fizzbuzzer(number);
+        //Then
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    void shouldReturnBuzzWithNumber5(){
+        //Given
+        int number = 5;
+        String expected = "Buzz";
 
         //When
         String actual = FizzBuzz.fizzbuzzer(number);
